d06fab709057e86c6788ed717f91034afe2bda7c
Leland Richardson <leland@tech.pro>
2014-11-24 22:28:50 -0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+
+gulp.task('browserify', function() {
+    gulp.src('src/js/main.js')
+      .pipe(browserify({ transform: 'reactify' }))
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('src/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copy', function() {
+    gulp.src('src/index.html')
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['browserify', 'copy', 'sass']);
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.*', ['default']);
+});
---
+++ src/js/components/app.js
@@ -0,0 +1,14 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var Viki = React.createClass({
+
+	render: function() {
+		return (
+			<h2>this is viki</h2>
+		);
+	}
+
+});
+
+module.exports = Viki;
