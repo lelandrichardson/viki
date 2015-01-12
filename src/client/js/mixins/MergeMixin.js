c2de6c3eddbbe3763cfbb54b946c98117c12ef20
Leland Richardson <leland@tech.pro>
2015-01-11 23:34:19 -0800

    added some useful mixing

--- src/client/js/mixins/MergeMixin.js
+++ src/client/js/mixins/MergeMixin.js
@@ -1,7 +1,16 @@
 var merge = require('deepmerge');
 
-return {
-    mergeState: function () {
-        return merge()
+var MergeMixin = {
+    mergeState: function ( ...props ) {
+
+        // NOTE: this WILL work in ES6, but the JSX harmony compiler is choking on it still
+        //this.state = merge(this.state, ...props);
+
+        props.unshift(this.state);
+        this.state = merge.apply(null, args);
+
+        this.forceUpdate();
     }
 };
+
+module.exports = MergeMixin;
