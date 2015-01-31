2640a5a2af5ed6b984c1d2677fbd6e34fdb7e94f
Leland Richardson <leland@tech.pro>
2015-01-30 20:37:48 -0800

    some not-so-sure-about-it "isomorphic" stuff

--- src/server/controllers/core.controller.js
+++ src/server/controllers/core.controller.js
@@ -2,10 +2,28 @@ var mongoose = require('mongoose');
 var Page = mongoose.model('Page');
 var Item = mongoose.model('Item');
 
+var React = require('react');
+var VikiApp = React.createFactory(require('../../client/js/components/VikiApp'));
+var Router = require('react-router');
+var Routes = require('../../client/js/routes');
+
 module.exports = {
 
     index: function ( req, res ) {
-        res.render('index', { request: req });
+
+
+        Router.run(routes, req.path, function ( Handler, state ) {
+
+
+            Flux.hydrate(res.dehydrate());
+
+            var reactOutput = React.renderToString(VikiApp({}));
+
+            res.render('index', { request: req, reactOutput: reactOutput });
+
+        });
+
+
     },
 
     pageDetail: function ( req, res ) {
