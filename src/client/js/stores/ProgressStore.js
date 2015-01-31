218e36e80803bcd4a7e681d3e7f473e397ac024c
Leland Richardson <leland@tech.pro>
2015-01-30 20:37:57 -0800

    progress store

--- src/client/js/stores/ProgressStore.js
+++ src/client/js/stores/ProgressStore.js
@@ -3,7 +3,7 @@ var Flux = require('react-flux');
 var ProgressStore = Flux.createStore({
 
     trackProgressFor: function ( constant ) {
-        this.state.set(constnt, true);
+        this.state.set(constant, true);
 
         this.addActionHandler(constant, {
             getInitialState: function () {
@@ -13,9 +13,11 @@ var ProgressStore = Flux.createStore({
             },
             before: function () {
                 this.setState({ isInProgress: true });
+                this.emit('CHANGE$' + constant);
             },
             after: function () {
                 this.setState({ isInProgress: false });
+                this.emit('CHANGE$' + constant);
             }
         });
     },
@@ -29,6 +31,35 @@ var ProgressStore = Flux.createStore({
             this.trackProgressFor(constant);
         }
         return this.getActionState(constant, 'isInProgress');
+    },
+
+    mixinFor: function ( constants ) {
+        var self = this;
+        return {
+            componentWillMount: function () {
+                if (typeof this._react_flux_onChange == "undefined") {
+                    this._react_flux_onChange = function () {
+                        if (!this.isMounted()) {
+                            return;
+                        }
+                        this.setState(this.getStateFromStores());
+                    }.bind(this);
+                }
+                this.setState(this.getStateFromStores());
+            },
+
+            componentDidMount: function () {
+                for (var i = 0; i < constants.length; i++) {
+                    self[i].on('CHANGE$' + constant, this._react_flux_onChange);
+                }
+            },
+
+            componentWillUnmount: function () {
+                for (var i = 0; i < constants.length; i++) {
+                    self[i].off('CHANGE$' + constant, this._react_flux_onChange);
+                }
+            }
+        }
     }
 });
 
